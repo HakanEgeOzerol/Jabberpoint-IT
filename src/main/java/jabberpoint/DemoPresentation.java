b6b1b28e45df7a266ab4f6afac9c32fbfb1ccf6b
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2025-03-05 21:24:36 +0100

    Updated project's file structure

--- src/main/java/jabberpoint/DemoPresentation.java
+++ src/main/java/jabberpoint/DemoPresentation.java
@@ -8,7 +8,7 @@
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 
-class DemoPresentation extends Accessor {
+public class DemoPresentation extends Accessor {
 
 	public void loadFile(Presentation presentation, String unusedFilename) {
 		presentation.setTitle("Demo Presentation");
